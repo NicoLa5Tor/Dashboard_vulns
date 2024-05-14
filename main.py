ac9d797be6a335ba65232137c012e8e38a6e0ef1
jdmooreno211 <jdmorenobeltran@gmail.com>
2024-05-14 11:45:52 -0500

    add styles and change view options

--- main.py
+++ main.py
@@ -9,18 +9,21 @@ def index():
     return render_template('index.html')
 
 @app.route('/data')
+
 def data():
     datos = {
         'labels': ['PC 1', 'PC 2', 'PC 3', 'PC 4', 'PC 5', 'PC 6', 'PC 7', 'PC 8', 'PC 9', 'PC 10'],
         'datasets': [
-            {'label': 'Low', 'data': [100, 400, 300, 200, 100, 400, 200, 600, 100, 200], 'backgroundColor': 'rgba(255, 99, 132, 0.5)', 'borderColor': 'rgba(255, 99, 132, 1)', 'borderWidth': 1},
-            {'label': 'Medium', 'data': [200, 300, 100, 400, 300, 700, 400, 300, 200, 100], 'backgroundColor': 'rgba(54, 162, 235, 0.5)', 'borderColor': 'rgba(54, 162, 235, 1)', 'borderWidth': 1},
-            {'label': 'High', 'data': [500, 100, 200, 200, 200, 100, 100, 200, 300, 400], 'backgroundColor': 'rgba(255, 206, 86, 0.5)', 'borderColor': 'rgba(255, 206, 86, 1)', 'borderWidth': 1},
-            {'label': 'Critical', 'data': [400, 600, 100, 300, 600, 200, 900, 1100, 200, 500], 'backgroundColor': 'rgba(75, 192, 192, 0.5)', 'borderColor': 'rgba(75, 192, 192, 1)', 'borderWidth': 1},
-            {'label': 'Sin Catalogar', 'data': [700, 600, 500, 300, 400, 300, 300, 500, 400, 300], 'backgroundColor': 'rgba(153, 102, 255, 0.5)', 'borderColor': 'rgba(153, 102, 255, 1)', 'borderWidth': 1}
+            {'label': 'Critical', 'data': [400, 600, 100, 300, 600, 200, 900, 1100, 200, 500], 'backgroundColor': 'rgba(0, 0, 0, 0.6)', 'borderColor': 'rgba(0, 0, 0, 1)', 'borderWidth': 1},
+            {'label': 'High', 'data': [500, 100, 200, 200, 200, 100, 100, 200, 300, 400], 'backgroundColor': 'rgba(220, 53, 69, 0.6)', 'borderColor': 'rgba(220, 53, 69, 1)', 'borderWidth': 1},
+            {'label': 'Medium', 'data': [200, 300, 100, 400, 300, 700, 400, 300, 200, 100], 'backgroundColor': 'rgba(255, 159, 64, 0.6)', 'borderColor': 'rgba(255, 159, 64, 1)', 'borderWidth': 1},
+            {'label': 'Low', 'data': [100, 400, 300, 200, 100, 400, 200, 600, 100, 200], 'backgroundColor': 'rgba(255, 193, 7, 0.6)', 'borderColor': 'rgba(255, 193, 7, 1)', 'borderWidth': 1},
+            {'label': 'Sin Catalogar', 'data': [700, 600, 500, 300, 400, 300, 300, 500, 400, 300], 'backgroundColor': 'rgba(128, 128, 128, 0.6)', 'borderColor': 'rgba(128, 128, 128, 1)', 'borderWidth': 1}
+
         ]
     }
     return jsonify(datos)
+
 
 @app.route('/data2')
 def data2():
